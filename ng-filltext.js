c2332b77f44a3fdbfbeb7054d66630478d45c196
Joe Maddalone <joe@insert-title.com>
2013-11-03 16:01:36 -0600

    change get to create for better naming in upcoming features

--- ng-filltext.js
+++ ng-filltext.js
@@ -1,35 +1,39 @@
-;angular.module('ng-filltext',[]).
-service('filltext',['$http',function($http){
-	return {
-		get:function(opts,callback) {
-			opts.callback = "JSON_CALLBACK"
-			opts.rows = opts.rows || 5
-				var config = {}
-				config["params"] = opts
-				$http.jsonp("http://www.filltext.com", config, {}).success(function (data) {
-    			callback(data)
-			});
-		}
-	}
+;
+angular.module('ng-filltext', []).
+service('filltext', ['$http', function($http) {
+    return {
+        url:"http://www.filltext.com/",
+        create: function(opts, callback) {
+            opts.callback = "JSON_CALLBACK";
+            opts.rows = opts.rows || 5;
+            var config = {};
+            config["params"] = opts;
+            $http.jsonp(this.url, config)
+            .success(function(data,status) {
+                callback(data,status);
+            }).
+            error(function(data,status){
+               callback(data,status); 
+            });
+        }
+    }
 }]).
-directive('filltextTable',function(filltext){
-	return {
-		restrict:"C",
-		scope: {
-     		item: "=",
-     		key: "="
-  		},
-		link:function($scope,element,attributes){
-			filltext.get($scope.$eval(attributes.dataset),function(data){
-				$scope.FTData=data;
-				$scope.order=$scope.$eval(attributes.order);
-				$scope.fto = function(val){
-					return $scope.order.indexOf(val)
-				}
-			});
-		},
-		template:'<tr ng-repeat="item in FTData">'+
-				'<td ng-repeat="key in order">{{item[key]}}</td>' +
-				'</tr>'
-	}
-});
+directive('filltextTable', function(filltext) {
+    return {
+        restrict: "C",
+        scope: {
+            item: "=",
+            key: "="
+        },
+        link: function($scope, element, attributes) {
+            filltext.create($scope.$eval(attributes.dataset), function(data) {
+                $scope.FTData = data;
+                $scope.order = $scope.$eval(attributes.order);
+                $scope.fto = function(val) {
+                    return $scope.order.indexOf(val)
+                }
+            });
+        },
+        template: '<tr ng-repeat="item in FTData">' + '<td ng-repeat="key in order">{{item[key]}}</td>' + '</tr>'
+    }
+}); 
